bd0f9c78791e8af08c8a69891d6b2cd05a62cec7
Ermane Veillard <65183512+mamanodev@users.noreply.github.com>
2025-02-16 03:19:20 -0500

    Finalisation du projet

---
+++ AcheterTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NUnit.Framework;
+
+namespace Projet03
+{
+    [TestFixture]
+    internal class AcheterTest
+    {
+        FrmPrincipale form = new FrmPrincipale();
+        [Test]
+        public void CalculRemisTest()
+        {
+
+            // Arrange
+
+            FrmPrincipale.totalMonnaie = 2.00m;
+            FrmPrincipale.Prix_bonbonChoisi = 1.50m;
+            decimal remis = 0.50m;
+            decimal retourCalculRemis = 0;
+            // Act
+            retourCalculRemis= form.calculRemis(FrmPrincipale.totalMonnaie, FrmPrincipale.Prix_bonbonChoisi);
+
+            // Assert
+            Assert.That(remis, Is.EqualTo(retourCalculRemis));
+        }
+
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -13,9 +13,11 @@ namespace Projet03
     public partial class FrmPrincipale : Form
     {
         //on récupère ici le bonbon que l'utilisateur a séléctionné une fois la selection est valide.
-        private static string Nom_bonbonChoisi;
-        private static decimal Prix_bonbonChoisi;
-        private static int Stock_bonbonChoisi;
+        public static string Nom_bonbonChoisi;
+        public static decimal Prix_bonbonChoisi;
+        public static int Stock_bonbonChoisi;
+        public static decimal totalMonnaie = 0;
+        public List<decimal> monnaiesPossible = new List<decimal> { 0.05m, 0.1m, 0.25m, 1, 2 };
 
         public FrmPrincipale()
         {
@@ -26,7 +28,10 @@ namespace Projet03
             //on désactive les composants mentionnés dans les étapes (voir énoncé)
             //exemple
             cmdAjouter.Enabled = false;
+            cmdAcheter.Enabled = false;
             lblMessage.Visible = false;
+            lblBonbon.Visible = false;
+            TxtMonnaie.Enabled = false;
             //...
 
         }
@@ -38,11 +43,40 @@ namespace Projet03
 
         private void cmdAjouter_Click(object sender, EventArgs e)
         {
-            
-        }
+            if (decimal.TryParse(TxtMonnaie.Text, out decimal monnaie) && monnaiesPossible.Contains(monnaie))
+            {
+                totalMonnaie += monnaie;
+                lblPercu.Text = Convert.ToString(totalMonnaie);
+                TxtMonnaie.Text="0";
+            }
+            else
+            {
+                MessageBox.Show("Veuillez entrer une monnaie valide (5c, 10c, 25c, 1$, 2$)", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                TxtMonnaie.Text = "0";
+            }
 
+        }
+        public decimal calculRemis(decimal monnaie, decimal PrixBonbon)
+        {
+            decimal remis = totalMonnaie - Prix_bonbonChoisi;
+            return remis;
+        }
         private void cmdAcheter_Click(object sender, EventArgs e)
         {
+            
+            if (totalMonnaie >= Prix_bonbonChoisi)
+            {
+                decimal MonnaieRemise = calculRemis(totalMonnaie, Prix_bonbonChoisi);
+                totalMonnaie = 0;
+                lblRemis.Text = Convert.ToString(MonnaieRemise);
+                lblMessage.Visible = true;
+                lblBonbon.Visible = true;
+                lblBonbon.Text = Nom_bonbonChoisi.ToUpper();
+            }
+            else
+            {
+                MessageBox.Show("Monnaie insuffisante, Ajouté de la monnaie pour completer l'achat", "Info", MessageBoxButtons.OK);
+            }
 
         }
 
@@ -53,6 +87,11 @@ namespace Projet03
         //bouton vérifier stock: fonction de vérification de la selection puis le stock
         private void cmdVerifierStock_Click(object sender, EventArgs e)
         {
+            if (TxtChoix.Text=="")
+            {
+                TxtChoix.Text="0";
+            }
+
             if ((int.Parse(TxtChoix.Text) < 1) || (int.Parse(TxtChoix.Text) > 25))
             {
                 MessageBox.Show("selection du bonbon invalide, il faut choisir de 1 à 25","Erreur");
@@ -66,6 +105,17 @@ namespace Projet03
                 //on vérifie si le bonbon choisi est en stock, sinon on affiche un MessageBox
                 //...
                 //si oui, on active les autres composants (voir énoncé)
+                if (selection == 0) {
+                    MessageBox.Show("Le bonbon n'est pas en stock !","Info");
+                } else {
+                  
+                    cmdAjouter.Enabled = true;
+                    cmdAcheter.Enabled = true;
+                    TxtMonnaie.Enabled = true;
+                    lblSelection.Text =Convert.ToString( selection);
+                    lblPrix.Text = Prix_bonbonChoisi.ToString();
+                }
+                
             }
 
 
@@ -74,6 +124,25 @@ namespace Projet03
         private void cmdRecommencer_Click(object sender, EventArgs e)
         {
             //remettre la machine à zéro
+            
+            if (totalMonnaie > 0)
+            {
+                MessageBox.Show($"Monnaie rendue : {totalMonnaie:C}", "Remboursement", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            totalMonnaie = 0;
+            TxtChoix.Text = "0";
+            TxtMonnaie.Text = "0";
+            //on désactive les composants mentionnés dans les étapes (voir énoncé)
+            //exemple
+            cmdAjouter.Enabled = false;
+            cmdAcheter.Enabled = false;
+            lblMessage.Visible = false;
+            lblBonbon.Visible = false;
+            TxtMonnaie.Enabled = false;
+            lblPrix.Text="00";
+            lblPercu.Text="00";
+            lblRemis.Text="00";
+            lblSelection.Text="00";
         }
 
         private void cmdQuitter_Click(object sender, EventArgs e)
